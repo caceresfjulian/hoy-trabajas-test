13dd68c1762cce8dd9242530de36e6f2c7587621
caceresfjulian <caceresf.julian@gmail.com>
2024-01-04 14:06:27 -0500

    Add banks MVC files and READ

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,7 +1,30 @@
 class BanksController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_bank_params, only: %i[show edit update destroy]
+  
   def index
-    render
+    @banks = Bank.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def set_bank_params
+    @bank = Bank.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "home#index"
-  get "banks", to: "banks#index"
+  resources :banks
 end
