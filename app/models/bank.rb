0fc833caf5f9d494d6e71349a5200eae0e150fb3
caceresfjulian <caceresf.julian@gmail.com>
2024-01-05 11:31:48 -0500

    Update model-side validations

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,3 @@
 class Bank < ApplicationRecord
-  validates :name, length: { in: 1..50 }, uniqueness: { case_sensitive: false }, presence: true
+  validates :name, length: { maximum: 50 }, uniqueness: { case_sensitive: false }, presence: true
 end
