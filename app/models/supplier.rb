83a112c98e2b0989f536cf8caaf5c67a91d8a98e
caceresfjulian <caceresf.julian@gmail.com>
2024-01-05 11:29:56 -0500

    Add model-side validations

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,6 @@
 class Supplier < ApplicationRecord
   validates :name, :nit, :contact_name, presence: true
-  validates :name, length: { minimum: 1 }
+  validates :nit, format: { with: /\b\d{9}-\d{1}\b|\b\d{9}\b/, message: "should have 9 digits and optionally a dash (-) and a number"}
+  validates :contact_phone, { length: { maximum: 10 }}
+  validates :account_number, { length: { maximum: 15 }}
 end
